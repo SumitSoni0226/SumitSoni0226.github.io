c833fba49eb24c85af25fb9677a8ffe419f76649
Sumit Soni <57682303+SumitSoni0226@users.noreply.github.com>
2019-11-23 18:09:42 +0530

    Add files via upload
    new commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('ul li a').click(function() {
+             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+             && location.hostname == this.hostname) {
+                      var $target = $(this.hash); $target = $target.length && $target 
+                      || $('[name=' + this.hash.slice(1) +']'); 
+                      if ($target.length) {
+                               var targetOffset = $target.offset().top; 
+                               $('html,body').animate({scrollTop: targetOffset}, 2000); 
+                               return false; 
+                       }
+               }
+       });
+});
